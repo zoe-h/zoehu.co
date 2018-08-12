a0a1f4b5fa86227737d1efac16eb7aef1ab2477f
zoe-h <30714392+zoe-h@users.noreply.github.com>
2018-08-12 20:35:12 +1000

    Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,21 @@
 
 $(document).ready(function() {
 
-// Add jQuery here
+  // Name typewriter
+
+  $('.typewriter_name').typeIt({
+       speed: 50,
+       deleteSpeed: 50,
+       cursor: false,
+       autoStart: true,
+       loop: true,
+  })
+  .tiType('ZOE WHO?')
+  .tiPause(1000)
+  .tiDelete(4)
+  .tiPause(200)
+  .tiType('HU.')
+  .tiPause(2000)
 
 // LOOP THROUGH MULTIPLE TYPEFACES
 
@@ -22,68 +36,63 @@ $(document).ready(function() {
     });
 
 
-// Instagram shenanigans
+// hover effects
+function isTouchDevice(){
+    return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+}
 
-  $(function () {
-    $('.instagram-link').on('click', function () {
-      $('<span>See you on the other side :-)</span>').appendTo('.instagram');
-      $('.instagram-link').off("mouseover");
-      $('.instagram-link').off("mouseleave");
-    });
-  });
+if(isTouchDevice()===true) {
 
-  $(function () {
-    $('.instagram-link').on('mouseover', function () {
-      $('<span>Go on... </span>').appendTo('.instagram');
-    });
-  });
-
-  $(function () {
-    $('.instagram-link').on('mouseleave', function () {
-      $('<span>Go on... </span>').appendTo('.instagram');
-    });
-  });
-
-// Email shenanigans
-
-$(function () {
-  $('.email-link').on('click', function () {
-    $('<span>Yay!</span>').appendTo('.email');
-    $('.email-link').off("mouseover");
-    $('.email-link').off("mouseleave");
-  });
-});
-
-$(function () {
-  $('.email-link').on('mouseover', function () {
-    $('<span>Say hi... </span>').appendTo('.email');
-  });
-});
-
-$(function () {
-  $('.email-link').on('mouseleave', function () {
-    $('<span>Say hi... </span>').appendTo('.email');
-  });
-});
-
-  // Name typewriter
-
-  $('.typewriter_name').typeIt({
-       speed: 50,
-       deleteSpeed: 50,
-       cursor: false,
-       autoStart: true,
-       loop: true,
-  })
-  .tiType('ZOE WHO?')
-  .tiPause(1000)
-  .tiDelete(4)
-  .tiPause(200)
-  .tiType('HU.')
-  .tiPause(2000)
+}
+else {
 
 
 
+  // Instagram shenanigans
+
+      $(function () {
+        $('.instagram-link').on('click', function () {
+          $('<span>See you on the other side :-)</span>').appendTo('.instagram');
+          $('.instagram-link').off("mouseover");
+          $('.instagram-link').off("mouseleave");
+        });
+      });
+
+      $(function () {
+        $('.instagram-link').on('mouseover', function () {
+          $('<span>Go on... </span>').appendTo('.instagram');
+        });
+      });
+
+      $(function () {
+        $('.instagram-link').on('mouseleave', function () {
+          $('<span>Go on... </span>').appendTo('.instagram');
+        });
+      });
+
+      // Email shenanigans
+
+      $(function () {
+        $('.email-link').on('click', function () {
+          $('<span>Yay!</span>').appendTo('.email');
+          $('.email-link').off("mouseover");
+          $('.email-link').off("mouseleave");
+        });
+      });
+
+      $(function () {
+        $('.email-link').on('mouseover', function () {
+          $('<span>Say hi... </span>').appendTo('.email');
+        });
+      });
+
+      $(function () {
+        $('.email-link').on('mouseleave', function () {
+          $('<span>Say hi... </span>').appendTo('.email');
+        });
+      });
+
+}
 
 
 
