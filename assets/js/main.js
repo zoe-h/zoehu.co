be04d0edfeba8f9466b5bc5cffafb81ee8db6be0
zoe-h <30714392+zoe-h@users.noreply.github.com>
2019-04-02 22:48:59 +1100

    2019

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,113 +1,19 @@
+/*
+ * main js
+ */
+
+$(document).ready(function () {
 
 
-$(document).ready(function() {
 
-  // Name typewriter
-
-  $('.typewriter_name').typeIt({
-       speed: 50,
-       deleteSpeed: 50,
-       cursor: false,
-       autoStart: true,
-       loop: true,
-  })
-  .tiType('ZOE WHO?')
-  .tiPause(1000)
-  .tiDelete(4)
-  .tiPause(200)
-  .tiType('HU.')
-  .tiPause(2000)
-
-// LOOP THROUGH MULTIPLE TYPEFACES
-
-  var fonts = ["Helvetica", "Times"];
-  var index = 0;
-    $("html").click(function() {
-      index++;
-      if (index >= fonts.length)
-        index = 0;
-
-      $("body").css("font-family", fonts[index]);
-
+    $('.info-button p').click(function () {
+        $('.info-panel').removeClass('hide');
     });
 
-    $("a").click(function() {
-      index--;
+    $('.info-close p').click(function () {
+        $('.info-panel').addClass('hide');
+
     });
-
-
-// hover effects
-function isTouchDevice(){
-    return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
-}
-
-if(isTouchDevice()===true) {
-
-}
-else {
-
-
-
-  // Instagram shenanigans
-
-      $(function () {
-        $('.instagram-link').on('click', function () {
-          $('<span>See you on the other side :-)</span>').appendTo('.instagram');
-          $('.instagram-link').off("mouseover");
-          $('.instagram-link').off("mouseleave");
-        });
-      });
-
-      $(function () {
-        $('.instagram-link').on('mouseover', function () {
-          $('<span>Go on... </span>').appendTo('.instagram');
-        });
-      });
-
-      $(function () {
-        $('.instagram-link').on('mouseleave', function () {
-          $('<span>Go on... </span>').appendTo('.instagram');
-        });
-      });
-
-      // Email shenanigans
-
-      $(function () {
-        $('.email-link').on('click', function () {
-          $('<span>Yay!</span>').appendTo('.email');
-          $('.email-link').off("mouseover");
-          $('.email-link').off("mouseleave");
-        });
-      });
-
-      $(function () {
-        $('.email-link').on('mouseover', function () {
-          $('<span>Say hi... </span>').appendTo('.email');
-        });
-      });
-
-      $(function () {
-        $('.email-link').on('mouseleave', function () {
-          $('<span>Say hi... </span>').appendTo('.email');
-        });
-      });
-
-}
-
-
-/**
-* Function that tracks a click on an outbound link in Analytics.
-* This function takes a valid URL string as an argument, and uses that URL string
-* as the event label. Setting the transport method to 'beacon' lets the hit be sent
-* using 'navigator.sendBeacon' in browser that support it.
-*/
-var trackOutboundLink = function(url) {
-   ga('send', 'event', 'outbound', 'click', url, {
-     'transport': 'beacon',
-     'hitCallback': function(){document.location = url;}
-   });
-}
-
 
 
 });
